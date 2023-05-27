6c03c57465faa943ee08359c323f98a1f856d41a
FARAH <farah.moh.awwad@gmail.com>
2023-05-27 14:45:15 +0300

    dashboard

---
+++ assets/scripts/dashboard.js
@@ -0,0 +1,5 @@
+usersArray = JSON.parse(localStorage.getItem("users"))
+    ? JSON.parse(localStorage.getItem("users"))
+    : [];
+let array = JSON.parse(localStorage.getItem("users"));
+console.log(usersArray)
