fe83e18b3d062a19fee2ca0238db44824911d78f
FARAH <farah.moh.awwad@gmail.com>
2023-05-27 15:47:08 +0300

    dashboard

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,5 +1,24 @@
 usersArray = JSON.parse(localStorage.getItem("users"))
-    ? JSON.parse(localStorage.getItem("users"))
+    ? JSON.parse(localStorage.getItem("currentUser"))
     : [];
-let array = JSON.parse(localStorage.getItem("users"));
+let array = JSON.parse(localStorage.getItem("currentUser"));
 console.log(usersArray)
+console.log(array.progressbar)
+
+
+let name_user =document.getElementById("name_user")
+let range =document.getElementById("range")
+name_user.textContent=`${array.firstName} ${array.secondName}`;
+
+
+if (array.progressbar==33)
+{
+    range.style.width='33%'
+}
+else if(array.progressbar==66){
+    range.style.width='66%'
+}
+else if(array.progressbar>66){
+    range.style.width='100%'
+}
+console.log(name);
